3e3756296e1180234cb8093861762f60f1026c44
Elvis Alves <elvis.alves54@gmail.com>
2021-09-04 02:46:46 -0300

    Create Context

---
+++ ApiAlunos/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ApiAlunos.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAlunos.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
